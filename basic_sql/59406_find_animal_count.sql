b1d48fa0870964a370b3fd09dba0b059fdf6f0fe
한중명 <gkswndaud@gmail.com>
2024-03-05 23:24:59 +0900

    sql day1

---
+++ basic_sql/59406_find_animal_count.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT 
+       COUNT(*)
+  FROM ANIMAL_INS
+
+--https://school.programmers.co.kr/learn/courses/30/lessons/59406
---
+++ basic_sql/59408_rm_dup.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT COUNT(DISTINCT NAME) AS count
+  FROM ANIMAL_INS
+ WHERE NAME IS NOT NULL
+
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59408
